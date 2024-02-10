191b6f29a6dbd45b495befca765775f206639137
Oleh <6083341@gmail.com>
2024-02-10 12:22:27 +0200

    chore: bring order to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          asd Learn React
-        </a>
-        <h1 className="text-5xl font-bold underline">Hello world!</h1>
-      </header>
+    <div>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
     </div>
   );
 }

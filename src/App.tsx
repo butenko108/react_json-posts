e1471f362863264e2f02e452e8642a3b875d53ec
Oleh <6083341@gmail.com>
2024-02-10 09:55:10 +0200

    chore: setup of eslint, prettier and tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,14 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          asd Learn React
         </a>
+        <h1 className="text-5xl font-bold underline">Hello world!</h1>
       </header>
     </div>
   );
